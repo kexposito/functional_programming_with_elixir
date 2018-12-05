15d5c93bf30a4bfe612a3a8783ecec61dab9eb11
Kevin Paul Exposito Coletta <kevin.exposito@fing.edu.uy>
2018-12-05 20:04:03 -0200

    prueba con hola.txt

--- Capitulo 2/checkout.ex
+++
@@ -1,5 +0,0 @@
-defmodule Checkout do 
-    def total_cost(price , tax_rate) do
-        price * (tax_rate + 1)
-    end
-end
--- Capitulo 3/ability_modifier.exs
+++
@@ -1,6 +0,0 @@
-user_input = IO.gets "Write your ability score:\n"
-{ability_score,_} = Integer.parse(user_input) # usuario ingresa numero donde se guardara en ability_score
-ability_modifier = ( ability_score - 10 ) / 2
-IO.puts "Your ability modifier is #{ability_modifier}" 
-
-# para correr este compilado se usa elixir ability_modifier.exs

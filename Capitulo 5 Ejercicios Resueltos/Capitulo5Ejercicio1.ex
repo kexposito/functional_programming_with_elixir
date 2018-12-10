eedbf1364b512fed798e07d93cad04aa5b96d742
kevin <kevin@mimiquate.com>
2018-12-10 15:14:21 -0300

    Capitulo 5 Ejercicio 1 , modificaciones por Marcelo

--- Capitulo 5 Ejercicios Resueltos/Capitulo5Ejercicio1.ex
+++ Capitulo 5 Ejercicios Resueltos/Capitulo5Ejercicio1.ex
@@ -10,25 +10,22 @@ defmodule EnchanterShop do
     @enchanter_name "Edwing"
 
     def enchant_for_sale([], _), do: []
-    def enchant_for_sale([item = %{magic: true} | incoming_item], func) do
-        
-        [item | enchant_for_sale(incoming_item, func)]
-    end
-
-
     def enchant_for_sale([item | incoming_item], func) do
-        [func.(item) | enchant_for_sale(incoming_item, func)] 
+        [func.(item) | enchant_for_sale(incoming_item, func)] # por ser func anonima . 
     end
 
+    def enchant_item(%{magic: false, price: price, title: title}) do
+        %{
+           title: "#{@enchanter_name}'s #{title}",
+            price: price * 3,
+            magic: true
+        } 
+    end
+    def enchant_item(item), do: item
 end
 
 
-IO.puts(" prueba ")
-IO.inspect(EnchanterShop.enchant_for_sale(EnchanterShop.data_test, 
-            fn item -> %{
-                            title: "Edwing's #{item.title}",
-                            price: item.price * 3,
-                            magic: true
-                        } 
-            end)
-        )
+IO.puts(" test ")
+IO.inspect(
+    EnchanterShop.enchant_for_sale(EnchanterShop.data_test, &EnchanterShop.enchant_item/1) 
+)

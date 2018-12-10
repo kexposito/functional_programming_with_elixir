7066733091a73d9fd0cabf480947e212a919122c
Kevin Paul Exposito Coletta <kevin.exposito@fing.edu.uy>
2018-12-10 21:02:57 -0200

    ejercicio 3 C5 terminado

--- Capitulo 5 Ejercicios Resueltos/Capitulo5Ejercicio3.ex
+++ Capitulo 5 Ejercicios Resueltos/Capitulo5Ejercicio3.ex
@@ -12,21 +12,18 @@
 defmodule Fibonacci do
 
 # fibonacci usando Stream.unfold
-
-    def fib(n) do
-       # Stream.unfold(0, fn # el primer parametro es el acumulador, el segundo es la funcion acumuladora
-            # 0 -> nil 
-             Stream.unfold({1, 1}, fn 
+    def fib(n) do# MAS INFO SOBRE UNFOLD http://www.petecorey.com/blog/2017/12/11/generating-sequences-with-elixir-streams/
+             Stream.unfold({1, 1}, fn # MAS INFO SOBRE UNFOLD https://blog.drewolson.org/elixir-streams
              {a, b} -> {a, {b, a + b}}
-            end)
+            end)|> Enum.take(n)
+    end
+
+    # Stream.unfold(0, fn # el primer parametro es el acumulador, el segundo es la funcion acumuladora
+            # 0 -> nil 
             # 1 -> 1
             # n -> {n, n + fib(n-1)}
         #end) |> Enum.to_list()
-    end
 
-    # Stream.unfold({1, 1}, fn 
-    # {a, b} -> {a, {b, a + b}}
-    # end)
 # fibonacci recursivo del Tip
     def fibRec(0), do: 0
     def fibRec(1), do: 1
@@ -42,11 +39,11 @@ defmodule Fibonacci do
     
 end
 
-# IO.puts "fib(0)"
-# IO.inspect(Fibonacci.fib(0)) 
+IO.puts "fib(0)"
+IO.inspect(Fibonacci.fib(0)) 
 
-# IO.puts "fib(1)"
-# IO.inspect(Fibonacci.fib(1)) 
+IO.puts "fib(1)"
+IO.inspect(Fibonacci.fib(1)) 
 
 IO.puts "fib(3)"
 IO.inspect(Fibonacci.fib(3)) 

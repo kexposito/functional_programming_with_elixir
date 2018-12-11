ada4530225ad098e3fcba1ce6ccac6bf4ab0169d
kevin <kevin@mimiquate.com>
2018-12-11 10:55:14 -0300

    Creacion de Dungenon Crawl

---
+++ Capitulo 6/dungeon_crawl/lib/dungeon_crawl/character.ex
@@ -0,0 +1,9 @@
+defmodule DungeonCrawl.Character do
+
+    defstruct name: nil,
+              desciption: nil,
+              hit_points: 0,
+              max_hit_points: 0,
+              attack_description: nil,
+              damage_range: nil
+end

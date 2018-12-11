25d90b9623b1f49da52de5937b758db19919e08b
kevin <kevin@mimiquate.com>
2018-12-11 11:56:15 -0300

    Actualizacion hasta la p 127

--- Capitulo 6/dungeon_crawl/lib/dungeon_crawl/character.ex
+++ Capitulo 6/dungeon_crawl/lib/dungeon_crawl/character.ex
@@ -1,7 +1,7 @@
 defmodule DungeonCrawl.Character do
 
     defstruct name: nil,
-              desciption: nil,
+              description: nil,
               hit_points: 0,
               max_hit_points: 0,
               attack_description: nil,
